8b94fc66fc682580e487a3da4c78deae3480bbaf
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-05-01 18:46:52 -0700

    feat: Set up printing of colored character
    Uses ANSI color codes to print a character

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,48 @@
 const std = @import("std");
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const AnsiColorCode = enum(u8) {
+    black = 0,
+    red = 1,
+    green = 2,
+    yellow = 3,
+    blue = 4,
+    magenta = 5,
+    cyan = 6,
+    white = 7,
+};
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+const AnsiColorType = enum(u8) {
+    dark_text = 3,
+    dark_bg = 4,
+    bright_text = 9,
+    bright_bg = 10,
+};
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+const AnsiColor = struct {
+    color: AnsiColorCode = AnsiColorCode.black,
+    type: AnsiColorType = AnsiColorType.dark_text,
 
-    try bw.flush(); // don't forget to flush!
+    fn val(self: *const AnsiColor) u8 {
+        return @intFromEnum(self.color) + @intFromEnum(self.type) * 10;
+    }
+};
+
+fn print_colored_char(
+    char: u8,
+    fgcolor: AnsiColor,
+    bgcolor: AnsiColor,
+) !void {
+    try std.io.getStdOut().writer().print("\x1b[{d};{d}m{c}\x1b[0m\n", .{
+        fgcolor.val(),
+        bgcolor.val(),
+        char,
+    });
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn main() !void {
+    try print_colored_char(
+        'z',
+        AnsiColor{ .color = AnsiColorCode.black, .type = AnsiColorType.dark_text },
+        AnsiColor{ .color = AnsiColorCode.red, .type = AnsiColorType.bright_bg },
+    );
 }
