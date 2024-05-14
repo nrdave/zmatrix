906841fa84146b31145df21a61b064c365c90e22
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-05-14 13:25:04 -0700

    chore: Add currently available Windows code
    Zig 0.12.0 doesn't have the SetConsoleMode function implemented in the
    standard library, but this PR - which was accepted recently
    (https://github.com/ziglang/zig/pull/18715) adds it. So, I just have to
    wait (don't want to use Zig master)

--- src/terminal_mode_control.zig
+++ src/terminal_mode_control.zig
@@ -3,11 +3,19 @@ const builtin = @import("builtin");
 
 const is_windows = builtin.os.tag == .windows;
 
-const TermStatus = if (is_windows) std.windows.DWORD else std.posix.termios;
+const TermStatus = if (is_windows) std.os.windows.DWORD else std.posix.termios;
 
 pub fn enableRawMode(input: std.fs.File.Handle) !TermStatus {
     if (is_windows) {
-        @compileError("Windows support not ready");
+        var orig_mode: TermStatus = undefined;
+        _ = std.os.windows.kernel32.GetConsoleMode(input, &orig_mode);
+        var t = orig_mode;
+        // This line clears the following flags
+        // 0x04: ENABLE_ECHO_INPUT
+        // 0x02: ENABLE_LINE_INPUT
+        // It also sets the ENABLE_VIRTUAL_TERMINAL_INPUT flag 0x200
+        t = t & ~(0x04 | 0x02) | 0x200;
+        @compileError("Windows Support not ready - waiting for Zig 0.13.0");
     } else {
         const orig_termios = try std.posix.tcgetattr(input);
         var t = orig_termios;
@@ -21,7 +29,7 @@ pub fn enableRawMode(input: std.fs.File.Handle) !TermStatus {
 
 pub fn restoreTermMode(input: std.fs.File.Handle, original_term_status: TermStatus) !void {
     if (is_windows) {
-        @compileError("Windows support not ready");
+        @compileError("Windows Support not ready - waiting for Zig 0.13.0");
     } else {
         try std.posix.tcsetattr(input, std.posix.TCSA.FLUSH, original_term_status);
     }
