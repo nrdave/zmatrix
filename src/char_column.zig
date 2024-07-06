6b23089eecd9424d17100c876d2f084ff486dcab
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-07-05 23:28:44 -0700

    Fix segfault in ColumnList.update
    Can't remove element from arraylist while iterating over said arraylist
    - indices get messed up. Have to iterate over indices, only increment if
      not removed

--- src/char_column.zig
+++ src/char_column.zig
@@ -65,10 +65,17 @@ pub const ColumnList = struct {
         matrix: *cm.CellMatrix,
         rng: std.rand.Random,
     ) !void {
-        self.counter +%= 1;
+        self.counter += 1;
         var char: u8 = '0';
         if (self.counter >= self.iterate_count) {
-            for (self.cols.items, 0..) |*col, i| {
+            // This method of removing elements from an ArrayList comes from jdh in this livestream:
+            // https://www.youtube.com/live/ajbYYgbDXGk?si=T6sL_hrrBfW--8bB&t=12609
+            // That said, I think I did it a tiny bit better (no struct member var)
+            var i: usize = 0;
+            var add_new_col = true;
+            while (i < self.cols.items.len) {
+                const col = &self.cols.items[i];
+                var remove = false;
                 // Printable ASCII chars range from 32 to 127, but 32 is
                 // space and 127 is delete. The other chars are visible
                 char = rng.intRangeLessThan(
@@ -78,17 +85,29 @@ pub const ColumnList = struct {
                 );
                 col.iterate(matrix, char);
 
-                if (col.tail > matrix.num_rows) {
-                    _ = self.cols.swapRemove(i);
-                } else if (col.tail > matrix.num_rows / 4) {
-                    try self.cols.append(createRandomColumn(
-                        self.column,
-                        matrix.num_rows,
-                        rng,
-                    ));
+                if (col.tail >= matrix.num_rows) {
+                    remove = true;
+                } else if (col.tail < matrix.num_rows / 4) {
+                    add_new_col = false;
                 }
+                if (remove) {
+                    _ = self.cols.swapRemove(i);
+                } else i += 1;
             }
+            self.counter = 0;
+            if (add_new_col)
+                try self.cols.append(createRandomColumn(
+                    self.column,
+                    matrix.num_rows,
+                    rng,
+                ));
         }
+        if (self.cols.items.len == 0)
+            try self.cols.append(createRandomColumn(
+                self.column,
+                matrix.num_rows,
+                rng,
+            ));
     }
     pub fn deinit(self: *ColumnList) void {
         self.cols.deinit();
